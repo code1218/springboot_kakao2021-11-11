cf377d38eb971de3f75e087f37b3d5d20460424d
junil <skjil1218@gmail.com>
2021-11-11 16:03:00 +0900

    kakaoboot commit

---
+++ springboot_kakao_junil/src/main/java/com/springboot/kakao/SpringbootKakaoJunilApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.kakao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKakaoJunilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKakaoJunilApplication.class, args);
+	}
+
+}
